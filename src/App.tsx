a058c90475f486d125b93c9b0a6a975a65eed8ea
jessicamelise <jessica.melise@hotmail.com>
2021-06-24 19:05:28 -0300

    implementação de contexto de autenticação para acessar o home e a sala

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
+import { AuhtContextProvider } from './contexts/AuthContext';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/rooms/new" component={NewRoom} />
+      <AuhtContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuhtContextProvider>
     </BrowserRouter>
   );
 }
