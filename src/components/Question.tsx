d31e7ad4da43199096a4048602c479d0e1e9bf90
jessicamelise <jessica.melise@hotmail.com>
2021-06-27 20:53:59 -0300

    implementação de método de dar destaque nas perguntas e de colocar como respondido

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import '../styles/question.scss';
+import classnames from 'classnames';
 
 type QuestionProps = {
     content: string;
@@ -7,12 +8,25 @@ type QuestionProps = {
         name: string;
         avatar: string;
     };
-    children?: ReactNode
+    children?: ReactNode;
+    isAnswered?: boolean;
+    isHighlighted?: boolean;
+
 }
 
-export function Question({ content, author, children }: QuestionProps) {
+export function Question({ 
+    content, 
+    author, 
+    children, 
+    isAnswered = false, 
+    isHighlighted = false }: QuestionProps) {
     return (
-        <div className="question">
+        // <div className={`question ${isAnswered ? 'answered' : ''}  ${isHighlighted ? 'highlighted' :  ''}`}>
+        <div className={classnames(
+            'question',
+            { answered: isAnswered },
+            { highlighted: isHighlighted && !isAnswered }
+        )}>
             <p>{content}</p>
             <footer>
                 <div className="user-info">
--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -2,6 +2,8 @@
 import { useHistory, useParams } from 'react-router-dom';
 import logoImg from '../assets/imagens/logo.svg';
 import deleteImg from '../assets/imagens/delete.svg';
+import checkImg from '../assets/imagens/check.svg';
+import answerImg from '../assets/imagens/answer.svg';
 import { Button } from '../components/Button';
 import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
@@ -35,6 +37,18 @@ export function AdminRoom() {
         }
     }
 
+    async function handleCheckQuestionAsAnswered(questionId: string) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isAnswered: true,
+        })
+    }
+
+    async function handleHighlightedQuestion(questionId: string) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isHighlighted: true,
+        });
+    }
+
     return (
         <div id="page-room">
             <header>
@@ -58,7 +72,19 @@ export function AdminRoom() {
                                 key={question.id}
                                 content={question.content}
                                 author={question.author}
+                                isAnswered={question.isAnswered}
+                                isHighlighted={question.isHighlighted}
                             >
+                                {!question.isAnswered && (
+                                    <>
+                                        <button type="button" onClick={() => handleCheckQuestionAsAnswered(question.id)}>
+                                            <img src={checkImg} alt="Marcar pergunta como respondida" />
+                                        </button>
+                                        <button type="button" onClick={() => handleHighlightedQuestion(question.id)}>
+                                            <img src={answerImg} alt="Dar destaque a pergunta" />
+                                        </button>
+                                    </>
+                                )}
                                 <button type="button" onClick={() => handleDeleteQuestion(question.id)}>
                                     <img src={deleteImg} alt="Remover pergunta" />
                                 </button>
